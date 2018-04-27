efb627c683b8f273985f336e871dbb7756ff15d5
Nate Miller <nate@natemiller.org>
2018-04-26 17:19:46 -0700

    Added capability to position at N,W,E,S

--- js/index.js
+++ js/index.js
@@ -7,13 +7,30 @@ class Point {
     }
 }
 
+class Rectangle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     * Returns the area of intersection with another rectangle
+     */
+    getIntersectionArea(rect) {
+        // trim away all regions not contained in the shape
+        // subtract this shape from the parameter shape
+    }
+}
+
 
 class PointMenu {
     constructor(elementId) {
         this.elementId = elementId;
         this.element = document.getElementById(elementId);
         this.origin = {};
-        this.stemLength = 40; // pixel length of connecting stem
+        this.stemLength = 80; // pixel length of connecting stem
     }
 
     set coordinates(point) {
@@ -35,7 +52,7 @@ class PointMenu {
 
         // who has the most space?
         // we want the best case of a) preferred quadrant b) no clipping
-        let availablePixels = {}
+        let availablePixels = {};
 
         // will the menu fit in the north quadrant?
         availablePixels.north = ptOrigin.y - boundingRect.height + originOffset;
@@ -43,9 +60,38 @@ class PointMenu {
         availablePixels.east = window.innerWidth - ptOrigin.x - boundingRect.width - originOffset;
         availablePixels.south = window.innerHeight - ptOrigin.y - boundingRect.height - originOffset;
 
+        // evaluate possible layouts
+        let rectNorth = new Rectangle(ptOrigin.x - (boundingRect.width / 2), 
+                                      ptOrigin.y - boundingRect.height - originOffset,
+                                      boundingRect.width,
+                                      boundingRect.height);
+
+        let rectWest = new Rectangle(ptOrigin.x - boundingRect.width - originOffset,
+                                     ptOrigin.y - (boundingRect.height / 2),
+                                     boundingRect.width,
+                                     boundingRect.height);
+
+        let rectEast = new Rectangle(ptOrigin.x + originOffset,
+                                     ptOrigin.y - (boundingRect.height / 2),
+                                     boundingRect.width,
+                                     boundingRect.height);
+
+        let rectSouth = new Rectangle(ptOrigin.x - (boundingRect.width / 2),
+                                      ptOrigin.y + originOffset,
+                                      boundingRect.width,
+                                      boundingRect.height);
 
 
-        // place the menu in the first available quadrant where no clipping shall occur
+        let renderRect = rectSouth;
+        this.coordinates = new Point(renderRect.x, renderRect.y);
+
+
+    }
+
+    /**
+     * Returns true when rectangle B fully subsumes rectangle A
+     */
+    doesRectContainRect(rectA, rectB) {
 
     }
 }
