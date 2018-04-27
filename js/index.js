6c35ba531f1dc1907a0ade7f13852a59a2d2264a
Nate Miller <nate@natemiller.org>
2018-04-27 08:03:57 -0700

    Rectangles can now calculate the area of overlapping regions

--- js/index.js
+++ js/index.js
@@ -18,10 +18,14 @@ class Rectangle {
     /**
      * Returns the area of intersection with another rectangle
      */
-    getIntersectionArea(rect) {
-        // trim away all regions not contained in the shape
-        // subtract this shape from the parameter shape
-    }
+     calcIntersectArea(rect) {
+        // Take the difference in x and y vector lengths
+        let dx = Math.min(this.x + this.width, rect.x + rect.width) - Math.max(this.x, rect.x);
+        let dy = Math.min(this.y + this.height, rect.y + rect.height) - Math.max(this.y, rect.y);
+
+        // Intersection exists if the difference is greater than zero
+        return Math.max(0, dx) * Math.max(0, dy);
+     }
 }
 
 
@@ -82,10 +86,15 @@ class PointMenu {
                                       boundingRect.height);
 
 
-        let renderRect = rectSouth;
+        let renderRect = rectNorth;
         this.coordinates = new Point(renderRect.x, renderRect.y);
 
 
+        let rectScreen = new Rectangle(0,0,window.innerWidth, window.innerHeight);
+        let intersectArea = rectScreen.calcIntersectArea(renderRect);
+        console.log("Screen:", rectScreen);
+        console.log("PoinMenu", renderRect);
+        console.log("Area:", intersectArea);
     }
 
     /**
