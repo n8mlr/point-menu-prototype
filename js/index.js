ad3553f01b7900327b0b5c76ab51caa5dce667fe
Nate Miller <nate@natemiller.org>
2018-04-27 08:21:29 -0700

    Tapping map will now place tip window in optimum location

--- js/index.js
+++ js/index.js
@@ -18,14 +18,14 @@ class Rectangle {
     /**
      * Returns the area of intersection with another rectangle
      */
-     calcIntersectArea(rect) {
+    calcIntersectArea(rect) {
         // Take the difference in x and y vector lengths
         let dx = Math.min(this.x + this.width, rect.x + rect.width) - Math.max(this.x, rect.x);
         let dy = Math.min(this.y + this.height, rect.y + rect.height) - Math.max(this.y, rect.y);
 
         // Intersection exists if the difference is greater than zero
         return Math.max(0, dx) * Math.max(0, dy);
-     }
+    }
 }
 
 
@@ -43,58 +43,60 @@ class PointMenu {
 
     openAtPoint(pt) {
         this.origin = pt;
-        this.calcAvailableQuadrant(pt, 
-                                   this.element.getBoundingClientRect(),
-                                   this.stemLength);
+        this.calcAvailableQuadrant(pt,
+            this.element.getBoundingClientRect(),
+            this.stemLength);
     }
 
     /**
      * Determines where the point menu should be rendered given the size of the menu and the origin of click
      */
     calcAvailableQuadrant(ptOrigin, boundingRect, originOffset) {
-        // determine the first available quadrant going in order of North, West, East, and South
+        let rectLayouts = [];
 
-        // who has the most space?
-        // we want the best case of a) preferred quadrant b) no clipping
-        let availablePixels = {};
-
-        // will the menu fit in the north quadrant?
-        availablePixels.north = ptOrigin.y - boundingRect.height + originOffset;
-        availablePixels.west = ptOrigin.x - boundingRect.width + originOffset;
-        availablePixels.east = window.innerWidth - ptOrigin.x - boundingRect.width - originOffset;
-        availablePixels.south = window.innerHeight - ptOrigin.y - boundingRect.height - originOffset;
-
-        // evaluate possible layouts
-        let rectNorth = new Rectangle(ptOrigin.x - (boundingRect.width / 2), 
-                                      ptOrigin.y - boundingRect.height - originOffset,
-                                      boundingRect.width,
-                                      boundingRect.height);
-
-        let rectWest = new Rectangle(ptOrigin.x - boundingRect.width - originOffset,
-                                     ptOrigin.y - (boundingRect.height / 2),
-                                     boundingRect.width,
-                                     boundingRect.height);
-
-        let rectEast = new Rectangle(ptOrigin.x + originOffset,
-                                     ptOrigin.y - (boundingRect.height / 2),
-                                     boundingRect.width,
-                                     boundingRect.height);
-
-        let rectSouth = new Rectangle(ptOrigin.x - (boundingRect.width / 2),
-                                      ptOrigin.y + originOffset,
-                                      boundingRect.width,
-                                      boundingRect.height);
+        // Calculate four possible regions for placement. The preferred order for usability on touchscreen
+        // devices is north, west, east, and south. Because the algorithm below will optimize for the last
+        // item added to rectLayouts[], the order of insertion into the array is important.
 
 
-        let renderRect = rectNorth;
-        this.coordinates = new Point(renderRect.x, renderRect.y);
+        // South
+        rectLayouts.push(new Rectangle(ptOrigin.x - (boundingRect.width / 2),
+            ptOrigin.y + originOffset,
+            boundingRect.width,
+            boundingRect.height));
+
+        // East
+        rectLayouts.push(new Rectangle(ptOrigin.x + originOffset,
+            ptOrigin.y - (boundingRect.height / 2),
+            boundingRect.width,
+            boundingRect.height));
+        
+        // West
+        rectLayouts.push(new Rectangle(ptOrigin.x - boundingRect.width - originOffset,
+            ptOrigin.y - (boundingRect.height / 2),
+            boundingRect.width,
+            boundingRect.height));
+
+        // North
+        rectLayouts.push(new Rectangle(ptOrigin.x - (boundingRect.width / 2),
+            ptOrigin.y - boundingRect.height - originOffset,
+            boundingRect.width,
+            boundingRect.height));
+
+        // Find the rectangle whose area of intersection with the screen is largest
+        let rectScreen = new Rectangle(0, 0, window.innerWidth, window.innerHeight);
+        let rectLargestArea = [null, 0];
+
+        for (var i = rectLayouts.length - 1; i >= 0; i--) {
+             let area = rectScreen.calcIntersectArea(rectLayouts[i]);
+             if (area > rectLargestArea[1]) {
+                rectLargestArea[0] = rectLayouts[i];
+                rectLargestArea[1] = area;
+             }
+        }
 
 
-        let rectScreen = new Rectangle(0,0,window.innerWidth, window.innerHeight);
-        let intersectArea = rectScreen.calcIntersectArea(renderRect);
-        console.log("Screen:", rectScreen);
-        console.log("PoinMenu", renderRect);
-        console.log("Area:", intersectArea);
+        this.coordinates = new Point(rectLargestArea[0].x, rectLargestArea[0].y);
     }
 
     /**
@@ -114,7 +116,7 @@ function onMapClick(e) {
 
 // Run
 let pm = new PointMenu("pm");
-pm.openAtPoint(500,500);
+
 
 let container = document.getElementById("container");
 addEventListener("click", onMapClick);
